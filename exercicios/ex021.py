88ca89a837f1c4cce22d02da1d76a96364bcb23b
Rafael Silva <rafaelsilva543@hotmail.com>
2025-04-19 14:55:17 -0300

    Update ex021.py

--- exercicios/ex021.py
+++ exercicios/ex021.py
@@ -1,3 +1,9 @@
 #Faça um programa em Python que abra e reproduza um arquivo de áudio em formato mp3.
-
+import pygame #usar o pip install pygame para instalar a biblioteca
+pygame.init() #inicializa o pygame
+pygame.mixer.init() #inicializa o mixer de som
+pygame.mixer.music.load('ex21.mp3') #carrega o arquivo mp3, DEVE estar na mesma pasta e ter o mesmo nome
+pygame.mixer.music.play() #reproduz o arquivo mp3
+input('Pressione Enter para parar a reprodução') #'enter' para a reprodução parar, sem o input nao funciona
+pygame.mixer.music.stop() #para a reprodução
 
