93f891f5c001d09bbd3b37e50c08547cf6802dbe
Rafael Silva <rafaelsilva543@hotmail.com>
2025-04-26 17:30:16 -0300

    Update ex021.py

--- exercicios/ex021.py
+++ exercicios/ex021.py
@@ -2,7 +2,7 @@
 import pygame #usar o pip install pygame para instalar a biblioteca
 pygame.init() #inicializa o pygame
 pygame.mixer.init() #inicializa o mixer de som
-pygame.mixer.music.load('ex21.mp3') #carrega o arquivo mp3, DEVE estar na mesma pasta e ter o mesmo nome
+pygame.mixer.music.load('ex021.mp3') #carrega o arquivo mp3, DEVE estar na mesma pasta e ter o mesmo nome
 pygame.mixer.music.play() #reproduz o arquivo mp3
 input('Pressione Enter para parar a reprodução') #'enter' para a reprodução parar, sem o input nao funciona
 pygame.mixer.music.stop() #para a reprodução
